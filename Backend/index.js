d968d8d8722cfc6c9f9c2e17222142468557788a
devanshubarman05 <devanshubarman05@gmail.com>
2025-01-21 00:11:09 +0530

    acxiom project

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-  res.send('BookStoreAPP!')
+  res.send('Library Management!')
 })
 
 app.listen(PORT, () => {
